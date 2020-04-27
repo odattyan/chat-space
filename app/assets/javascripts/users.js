0fb52b77f3d12d8e37f736deb1d1a40747ad332e
odattyan <62612364+odattyan@users.noreply.github.com>
2020-04-28 00:51:40 +0900

    UPdata users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,11 +18,18 @@ $(function() {
                </div>`
     search_user.append(html);
   }
-
-  $(".chat-group-form__field--right").on('click', "#user-search-field", function(){
-    
-  })
-
+  function addDeleteUser(name, id) {
+    var html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    var html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
   $('#user-search-field').on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -46,5 +53,20 @@ $(function() {
     .fail(function() {
       alert("ユーザー検索に失敗しました");
     });
+  });
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
   });
 });
