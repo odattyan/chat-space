751eeeea5ab22e6f7eb9d0c22d1c93cf1cbe1d60
odattyan <62612364+odattyan@users.noreply.github.com>
2020-04-29 07:25:25 +0900

    UPdata routes

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -69,15 +69,20 @@ $(function(){
         data: {id: last_message_id}
       })
       .done(function(messages) {
+      if (messages.length !== 0) {
         var insertHTML = '';
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
       $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
       })
       .fail(function() {
         alert('error');
       });
     };
-  setInterval(reloadMessages, 7000);
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
 });
