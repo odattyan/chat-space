85e3e6741a0e197d41686331659b30b24732849e
odattyan <62612364+odattyan@users.noreply.github.com>
2020-04-29 00:01:30 +0900

    Updata message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -59,5 +59,19 @@ $(function(){
       $('#new_message')[0].reset();
       $('#submit-btn').prop('disabled', false);
     });
+    var reloadMessages = function() {
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log('success');
+      })
+      .fail(function() {
+        alert('error');
+      });
   });
 });
