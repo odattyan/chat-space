cffe9e8d3e7f9986721ab4827eee6bc5a31925cd
odattyan <62612364+odattyan@users.noreply.github.com>
2020-05-02 02:08:49 +0900

    Updata message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -74,8 +74,8 @@ $(function(){
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.chat-main__message-list').append(insertHTML);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
       }
       })
       .fail(function() {
